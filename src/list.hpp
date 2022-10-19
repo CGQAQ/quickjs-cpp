51965728d04f5f26045e3850bf005ada560be6b7
Jason <m.jason.liu@outlook.com>
2022-10-20 00:25:34 +0800

    comment out foo

--- src/list.hpp
+++ src/list.hpp
@@ -6,24 +6,24 @@
 namespace qjs {
 using std::list;
 
-auto foo() {
-    list<int> a = list<int>();
+// auto foo() {
+//     list<int> a = list<int>();
 
-    //  // list_add
-    //  a.push_front(1);
-    //  // list_add_tail
-    //  a.push_back(2);
-    //  // list_del
-    //  a.remove(1);
-    //  // list_empty
-    //  a.clear();
-    //  // list_for_each
-    //  // std::for_each(a.begin(), a.end(), [](int i) {
-    //  //   do something
-    //  // });
-    //  // list_for_each_prev
-    //  // std::for_each(a.end(), a.begin(), [](int i) {
-    //  //   do something
-    //  // });
-}
+//      // list_add
+//      a.push_front(1);
+//      // list_add_tail
+//      a.push_back(2);
+//      // list_del
+//      a.remove(1);
+//      // list_empty
+//      a.clear();
+//      // list_for_each
+//      // std::for_each(a.begin(), a.end(), [](int i) {
+//      //   do something
+//      // });
+//      // list_for_each_prev
+//      // std::for_each(a.end(), a.begin(), [](int i) {
+//      //   do something
+//      // });
+//}
 }  // namespace qjs
