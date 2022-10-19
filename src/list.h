d0d119a61fe35d14ab3afd97cc0051294b4cb6ec
CGQAQ <m.jason.liu@outlook.com>
2022-10-19 16:28:23 +0800

    add clang-format & some comments

--- src/list.h
+++ src/list.h
@@ -2,3 +2,28 @@
 
 // we already have a doubly-linked list implementation in the standard library
 #include <list>
+
+namespace qjs {
+using std::list;
+
+auto foo() {
+  list<int> a = list<int>();
+
+  //  // list_add
+  //  a.push_front(1);
+  //  // list_add_tail
+  //  a.push_back(2);
+  //  // list_del
+  //  a.remove(1);
+  //  // list_empty
+  //  a.clear();
+  //  // list_for_each
+  //  // std::for_each(a.begin(), a.end(), [](int i) {
+  //  //   do something
+  //  // });
+  //  // list_for_each_prev
+  //  // std::for_each(a.end(), a.begin(), [](int i) {
+  //  //   do something
+  //  // });
+}
+}  // namespace qjs
