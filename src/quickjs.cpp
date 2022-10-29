4fc8eef6456f90c103cccb9b629e9d9327cea572
Jason <m.jason.liu@outlook.com>
2022-10-29 15:35:12 +0800

    init quickjs.cpp & quickjs.hpp

---
+++ src/quickjs.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Jason Liu on 2022/10/29.
+//
+
+#include "quickjs.hpp"
+
+namespace qjs {}  // namespace qjs
---
+++ src/quickjs.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Jason Liu on 2022/10/29.
+//
+
+#ifndef QUICKJS_CPP_QUICKJS_HPP
+#define QUICKJS_CPP_QUICKJS_HPP
+
+namespace qjs {
+
+class quickjs {};
+
+}  // namespace qjs
+
+#endif  // QUICKJS_CPP_QUICKJS_HPP
